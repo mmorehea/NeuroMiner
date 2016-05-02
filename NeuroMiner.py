3201157e18ab89c1cc3ea99232738c3c460ee6f3
Maher Shammaa <amshammaa@mix.wvu.edu>
2016-05-02 15:50:16 -0400

    separated the mining from the file grabbing function

--- NeuroMiner.py
+++ NeuroMiner.py
@@ -1,4 +1,4 @@
-"""Take a list of neuron names, and obtain the rest of the information for each one found on http://neuromorpho.org/neuron_info.jsp?neuron_name='name'."""
+"""Take a list of neuron names, and obtain the rest of the information for each one found on http://neuromorpho.org/neuron_info.jsp?neuron_name='name'. Also grab the standardized swc file for each cell."""
 # -*- coding: utf-8 -*-
 
 import numpy as np
@@ -12,6 +12,42 @@ import urllib2
 import os
 import pickle
 
+def mine(url):
+
+    a = pd.read_html(url, attrs={'id': 'NeuronInfotable12'})[0]
+    a = a.fillna(value='')
+    b = pd.DataFrame(a[1].values, index=a[0].values, columns=['Vals'])
+    b.index = [x[:-2] for x in b.index]
+
+    c = pd.read_html(url, attrs={'id': 'NeuronInfotable11'})[0]
+    c = c.fillna(value='')
+    d = pd.DataFrame(c[1].values, index=c[0].values, columns=['Vals'])
+    d.index = [x[:-2] for x in d.index]
+    k = pd.concat([b, d])
+
+    vals = []
+    for column in columns:
+        vals.append(k.ix[column, 'Vals'])
+    rows.append(vals)
+
+    return rows
+
+def grabFile(url, name):
+
+    links = BeautifulSoup(requests.get(url).text, 'lxml').find_all('a')
+    swc_link = [x for x in links if 'Morphology File (Standardized)' in str(x)][0].get('href')
+    swc_link = 'http://neuromorpho.org/' + swc_link
+
+    if not os.path.exists('swcs/'):
+        os.makedirs('swcs/')
+
+    rq = urllib2.Request(swc_link)
+    res = urllib2.urlopen(rq)
+    swc = open('swcs/' + name[28:] + '.swc', 'wb')
+    swc.write(res.read())
+    swc.close()
+
+    return
 
 url_template = 'http://neuromorpho.org/{name}'
 
@@ -58,46 +94,14 @@ columns += ["NeuroMorpho.Org ID", "Neuron Name", "Archive Name",
 total_cell_number = str(len(names))
 rows = []
 for cell_number, name in enumerate(names):
+    
     print "Getting " + name + ', cell ' + str(cell_number + 1) + ' / ' + total_cell_number
     url = url_template.format(name=name)
 
-    a = pd.read_html(url, attrs={'id': 'NeuronInfotable12'})[0]
-    a = a.fillna(value='')
-    b = pd.DataFrame(a[1].values, index=a[0].values, columns=['Vals'])
-    b.index = [x[:-2] for x in b.index]
+    rows = mine(url)
 
-    c = pd.read_html(url, attrs={'id': 'NeuronInfotable11'})[0]
-    c = c.fillna(value='')
-    d = pd.DataFrame(c[1].values, index=c[0].values, columns=['Vals'])
-    d.index = [x[:-2] for x in d.index]
-    k = pd.concat([b, d])
-
-    vals = []
-    for column in columns:
-        vals.append(k.ix[column, 'Vals'])
-    rows.append(vals)
-
-    links = BeautifulSoup(requests.get(url).text, 'lxml').find_all('a')
-    swc_link = [x for x in links if 'Morphology File (Standardized)' in str(x)][0].get('href')
-    swc_link = 'http://neuromorpho.org/' + swc_link
-
-    if not os.path.exists('swcs/'):
-        os.makedirs('swcs/')
-
-    rq = urllib2.Request(swc_link)
-    res = urllib2.urlopen(rq)
-    swc = open('swcs/' + name[28:] + '.swc', 'wb')
-    swc.write(res.read())
-    swc.close()
-
-
-# rows = np.c_[names, rows]
+    grabFile(url, name)
 
 frame = pd.DataFrame(np.array(rows), columns=columns)
-
-# html = frame.to_html()
-# with open('temp.html', 'w') as f:
-#     f.write(html.encode('UTF-16'))
-# webbrowser.open_new('temp.html')
 
 frame.to_csv('neuroData.csv', encoding='UTF-16')
